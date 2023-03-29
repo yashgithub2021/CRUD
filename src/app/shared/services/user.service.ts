57d6bc96f554c409497f5578c61b70f75a6d47e4
Your Name <you@example.com>
2023-03-29 13:22:11 +0530

    done

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = 'http://localhost:3000/data'
+
+  constructor(private http: HttpClient) { }
+
+  //Get Data
+  getData() {
+    return this.http.get(this.url)
+  }
+
+  // Add Data
+  addData(data: any) {
+    return this.http.post(this.url, data)
+  }
+
+  //Delete Data
+  delData(id: any) {
+    const baseUrl = `${this.url}/${id}`
+    return this.http.delete(baseUrl)
+  }
+
+  //getSingle User
+  getOneUser(id: any) {
+    const baseUrl = `${this.url}/${id}`
+    return this.http.get(baseUrl)
+  }
+}
